42c7976c752b1a142641245ce189402944dd75b9
Monique Alves <moniquealv27@gmail.com>
2025-03-07 13:33:14 -0300

    add readme

---
+++ script.js
@@ -0,0 +1,19 @@
+function togglemode() {
+    const html = document.documentElement
+
+if(html.classList.contains("light")) {
+        html.classList.remove("light")
+    } else {
+        html.classList.add("light")
+
+        //pegar a tag img
+        const img = document.querySelector("#profile img")
+        // substituir a imagem
+        if(html.classList.contains("light")){
+            img.setAttribute("src", "./assets./avatar-light.png")
+
+        } else{
+            img.setAttribute("src", "./assets./avatar.png")
+        }
+    }
+} 
